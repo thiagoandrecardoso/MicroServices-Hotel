4812b11a558200f5255f20e41255aee01948fc46
Thiago <thiagoandrecardoso@icloud.com>
2022-08-28 08:45:49 -0300

    Created
    Project Manager Hotel

---
+++ manager-hotel/src/main/java/com/example/managerhotel/ManagerHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.managerhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerHotelApplication.class, args);
+    }
+
+}
---
+++ manager-hotel/src/test/java/com/example/managerhotel/ManagerHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managerhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
