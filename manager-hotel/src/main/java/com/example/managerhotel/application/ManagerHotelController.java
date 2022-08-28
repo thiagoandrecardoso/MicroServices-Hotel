d032712eba2e48be43a0a53c593d09ca1a107524
Thiago <thiagoandrecardoso@icloud.com>
2022-08-28 11:53:09 -0300

    Added manager server and controller

--- manager-hotel/src/main/java/com/example/managerhotel/application/ManagerHotelController.java
+++ manager-hotel/src/main/java/com/example/managerhotel/application/ManagerHotelController.java
@@ -1,8 +1,14 @@
 package com.example.managerhotel.application;
 
+import com.example.managerhotel.application.exception.DataClientNotFoundException;
+import com.example.managerhotel.application.exception.ErrorCommMicroServiceException;
+import com.example.managerhotel.domain.model.ClientInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +21,19 @@ public class ManagerHotelController {
     @GetMapping
     public String status() {
         return "Ok";
+    }
+
+    @GetMapping(value = "client-info", params = "cpf")
+    public ResponseEntity getClientInfoByCpf(@RequestParam("cpf") String cpf) {
+        ClientInfo clientInfo = null;
+        try {
+            clientInfo = managerHotelService.getClientInfoByCpf(cpf);
+            return ResponseEntity.ok(clientInfo);
+        } catch (DataClientNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (ErrorCommMicroServiceException e) {
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
     }
 
 }
--- manager-hotel/src/main/java/com/example/managerhotel/application/ManagerHotelService.java
+++ manager-hotel/src/main/java/com/example/managerhotel/application/ManagerHotelService.java
@@ -1,13 +1,46 @@
 package com.example.managerhotel.application;
 
+import com.example.managerhotel.application.exception.DataClientNotFoundException;
+import com.example.managerhotel.application.exception.ErrorCommMicroServiceException;
+import com.example.managerhotel.domain.model.ClientInfo;
+import com.example.managerhotel.domain.model.DataClient;
+import com.example.managerhotel.domain.model.RoomClient;
 import com.example.managerhotel.infra.clients.HostResourceClient;
+import com.example.managerhotel.infra.clients.RoomResourceClient;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ManagerHotelService {
 
     private final HostResourceClient hostResourceClient;
+    private final RoomResourceClient roomResourceClient;
+
+    public ClientInfo getClientInfoByCpf(String cpf) throws DataClientNotFoundException,
+            ErrorCommMicroServiceException {
+        try {
+            ResponseEntity<DataClient> dataClientResponseEntity = hostResourceClient.dataClient(cpf);
+            ResponseEntity<List<RoomClient>> roomClientResponseEntity = roomResourceClient.getRoomByClient(cpf);
+
+            return ClientInfo
+                    .builder()
+                    .dataClient(dataClientResponseEntity.getBody())
+                    .roomClientList(roomClientResponseEntity.getBody())
+                    .build();
+
+        } catch (FeignException.FeignClientException e) {
+            int status = e.status();
+            if (HttpStatus.SC_NOT_FOUND == status) {
+                throw new DataClientNotFoundException();
+            }
+            throw new ErrorCommMicroServiceException(e.getMessage(), status);
+        }
+    }
 
 }
