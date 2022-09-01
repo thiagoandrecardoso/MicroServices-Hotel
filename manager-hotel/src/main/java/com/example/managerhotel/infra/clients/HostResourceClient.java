4de1143385b6fc49c877f35e0395478f7b15bf6f
Thiago <thiagoandrecardoso@icloud.com>
2022-09-01 15:12:13 -0300

    optimize imports

--- manager-hotel/src/main/java/com/example/managerhotel/infra/clients/HostResourceClient.java
+++ manager-hotel/src/main/java/com/example/managerhotel/infra/clients/HostResourceClient.java
@@ -1,11 +1,11 @@
 package com.example.managerhotel.infra.clients;
 
 
+import com.example.managerhotel.domain.model.DataClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.example.managerhotel.domain.model.DataClient;
 
 @FeignClient(value = "client", path = "/clients")
 public interface HostResourceClient {
