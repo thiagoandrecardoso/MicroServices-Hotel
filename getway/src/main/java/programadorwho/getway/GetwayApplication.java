00d31873f7d643dcd262482bd67aa0682e3e7adc
Thiago <thiagoandrecardoso@icloud.com>
2022-10-10 18:13:50 -0300

    Add project getway

---
+++ getway/src/main/java/programadorwho/getway/GetwayApplication.java
@@ -0,0 +1,25 @@
+package programadorwho.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetwayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder
+                .routes()
+                .route(r -> r.path("/clients/**").uri("lb://client"))
+                .route(r -> r.path("/cartoes/**").uri("lb://mscards"))
+                .route(r -> r.path("/manager-hotel/**").uri("lb://manager-hotel"))
+                .build();
+    }
+}
