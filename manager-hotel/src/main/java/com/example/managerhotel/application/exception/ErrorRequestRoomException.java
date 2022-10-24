1b8b8abd64c2fdf8b65c3c55c666e484d812054f
Thiago <thiagoandrecardoso@icloud.com>
2022-10-23 21:55:32 -0300

    Added class exception Request Room

---
+++ manager-hotel/src/main/java/com/example/managerhotel/application/exception/ErrorRequestRoomException.java
@@ -0,0 +1,7 @@
+package com.example.managerhotel.application.exception;
+
+public class ErrorRequestRoomException extends RuntimeException{
+    public ErrorRequestRoomException(String message) {
+        super(message);
+    }
+}
